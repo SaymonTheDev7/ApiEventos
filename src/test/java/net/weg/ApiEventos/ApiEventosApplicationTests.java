d9f6889aa17c753f6a7c8b9afe48f7fcdae21453
Saymon Oliveira de Castro <saymon_castro@estudante.sesisenai.org.br>
2024-11-26 14:18:22 -0300

    Initial commit

---
+++ src/test/java/net/weg/ApiEventos/ApiEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package net.weg.ApiEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
